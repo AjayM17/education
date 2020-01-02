add1b53c41fd06706698cd7a11794d05ceceb277
Madhur <arora.madhur8@gmail.com>
2020-01-02 20:58:13 +0530

    Content updation for about us and facilities

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -19,8 +19,8 @@ const Header = (props) => {
                 <li className={"list-item "   + (active ==='about' ? 'active' : '')} onClick={() => { setActive('about'); history.push('/about') }}>About Us</li>
                 <li className={"list-item "  + (active === 'courses' ? 'active' : '')} onClick={() => { setActive('courses'); history.push('/courses') }}>Courses</li>
                 <li className={"list-item "  + (active === 'admission' ? 'active' : '')} onClick={() => { setActive('admission'); history.push('/admission') }}>Admission</li>
-                <li className={"list-item "  + (active === 'message' ? 'active' : '')} onClick={() => { setActive('message'); history.push('/message') }}>Direct Message</li>
-                <li className={"list-item "  + (active === 'gallery' ? 'active' : '')} onClick={() => { setActive('gallery'); history.push('/gallery') }}>Gallary</li>
+                <li className={"list-item "  + (active === 'message' ? 'active' : '')} onClick={() => { setActive('message'); history.push('/message') }}>Director Message</li>
+                <li className={"list-item "  + (active === 'gallery' ? 'active' : '')} onClick={() => { setActive('gallery'); history.push('/gallery') }}>Gallery</li>
                 <li className={"list-item "  + (active === 'placement' ? 'active' : '')} onClick={() => { setActive('placement'); history.push('/placement') }}>Placement</li>
                 <li className={"list-item "  + (active === 'contact' ? 'active' : '')} onClick={() => { setActive('contact'); history.push('/contact') }}>Contact Us</li>
                 <li className={"list-item "  + (active === 'facilities' ? 'active' : '')} onClick={() => { setActive('facilities'); history.push('/facilities') }}>Facilities/ Methodology</li>
